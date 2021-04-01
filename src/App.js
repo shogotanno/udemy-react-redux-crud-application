b0b9005456d8f84653821bcecf02262b2800ad28
shogotanno <tanno.shogo@headwaters.co.jp>
2021-04-02 00:38:21 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 function App() {
-  return <div><h1>Hello,world!</h1></div>
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am Clicked!")}} />;
+    </React.Fragment>
+  )
 }
 
 export default App;
